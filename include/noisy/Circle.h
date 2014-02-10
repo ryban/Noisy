2b38c7c93dcb91f5586043dcea3bcec81b947da1
ryban <rvangilder1@gmail.com>
2014-02-10 13:59:05 -0800

    Added threshold to Circle module

--- include/noisy/Circle.h
+++ include/noisy/Circle.h
@@ -8,22 +8,25 @@ namespace noisy
 
 // Class that selects between two sources whether the input values lie
 // within a given radius of the origin or not. Is a circle in 2D, sphere in 3D.
-// Points on the edge of the radius are considerd inside.
 // Does not blend the sources together
 class Circle : public Module
 {
     public:
         Circle();
+        // Sets the thrshold to radius*0.05
         Circle(float radius, Module *inside, Module *outside);
+        Circle(float radius, Module *inside, Module *outside, float threshold);
 
         void setRadius(float r);
         void setSources(Module *inside, Module *outside);
+        void setThreshold(float threshold);
 
         virtual float getValue(float x, float y);
         virtual float getValue(float x, float y, float z);
     private:
         // the radius sqaured. sqrt is slow, so we avoid it
         float m_radiusSq;
+        float m_thresholdSq;
         Module *m_inside;
         Module *m_outside;
 };
--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,4 +1,5 @@
 #include "noisy/Circle.h"
+#include "noisy/Utils.h"
 
 namespace noisy
 {
@@ -14,6 +15,16 @@ Circle::Circle(float radius, Module *inside, Module *outside)
     m_radiusSq = radius*radius;
     m_inside = inside;
     m_outside = outside;
+    // default threshold is 1/20th of the radius
+    m_thresholdSq = m_radiusSq*0.05;
+}
+
+Circle::Circle(float radius, Module *inside, Module *outside, float threshold)
+{
+    m_radiusSq = radius*radius;
+    m_inside = inside;
+    m_outside = outside;
+    m_thresholdSq = threshold*threshold;
 }
 
 void Circle::setRadius(float r)
@@ -26,23 +37,34 @@ void Circle::setSources(Module *inside, Module *outside)
     m_inside = inside;
     m_outside = outside;
 }
-
+void Circle::setThreshold(float threshold)
+{
+    m_thresholdSq = threshold*threshold;
+}
 float Circle::getValue(float x, float y)
 {
     float r2 = x*x + y*y;
-    if(r2 > m_radiusSq)
+    if(r2 > m_radiusSq+m_thresholdSq)
         return m_outside->getValue(x, y);
-    else
+    else if(r2 < m_radiusSq-m_thresholdSq)
         return m_inside->getValue(x, y);
+    else
+        return utils::lerp( m_outside->getValue(x, y),
+                            m_inside->getValue(x, y),
+                            ((m_radiusSq+m_thresholdSq)-r2)/(m_thresholdSq*2));
 }
 
 float Circle::getValue(float x, float y, float z)
 {
     float r2 = x*x + y*y + z*z;
-    if(r2 > m_radiusSq)
+    if(r2 > m_radiusSq+m_thresholdSq)
         return m_outside->getValue(x, y, z);
-    else
+    else if(r2 < m_radiusSq-m_thresholdSq)
         return m_inside->getValue(x, y, z);
+    else
+        return utils::lerp( m_outside->getValue(x, y, z),
+                            m_inside->getValue(x, y, z),
+                            ((m_radiusSq+m_thresholdSq)-r2)/(m_thresholdSq*2));
 }
 
 }; // namespace noisy
